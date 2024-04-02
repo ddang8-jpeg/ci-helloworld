be36b5e5c32f1cd7f06d650fea960ab116845fc2
ddang8-jpeg <parkrangerdang@gmail.com>
2024-04-01 21:43:41 -0400

    Added test for countOf added

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -46,6 +46,40 @@ public class ArrayUtilsTest {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
 
+    @Test
+    public void testCountOf_TargetInArray() {
+        int[] arr = {1, 2, 3, 4, 1, 5};
+        int target = 1;
+        assertEquals(2, ArrayUtils.countOf(arr, target)); // Expecting 2 occurrences of 1
+    }
+
+    @Test
+    public void testCountOf_TargetNotInArray() {
+        int[] arr = {2, 3, 4, 5};
+        int target = 1;
+        assertEquals(0, ArrayUtils.countOf(arr, target)); // Expecting 0 occurrences of 1
+    }
+
+    @Test
+    public void testCountOf_EmptyArray() {
+        int[] arr = {};
+        int target = 3;
+        assertEquals(0, ArrayUtils.countOf(arr, target)); // Expecting 0 occurrences of 3
+    }
+
+    @Test
+    public void testCountOf_OnlyTargetInArray() {
+        int[] arr = {7, 7, 7, 7, 7};
+        int target = 7;
+        assertEquals(5, ArrayUtils.countOf(arr, target)); // Expecting 5 occurrences of 7
+    }
+
+    @Test
+    public void testCountOf_MultipleOccurrencesOfTarget() {
+        int[] arr = {2, 3, 4, 4, 2, 4, 1, 4};
+        int target = 4;
+        assertEquals(4, ArrayUtils.countOf(arr, target)); // Expecting 4 occurrences of 4
+    }
 
 
 }
